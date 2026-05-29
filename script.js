cf8b6f1000b38f9262f95fbd62fa3dda228e7570
zokosting <felipemunyoz@gmail.com>
2026-05-30 00:36:41 +0200

    Map updates

--- script.js
+++ script.js
@@ -50,6 +50,7 @@ const MAPAS_CONFIG = {
         { nombre: "Sands of Time", descripcion: "Sand desert with pyramids and a small river.<br/>Map size: 257 | Strat. points: 7 | Relics: 2 | Critical loc.: 3" },
         { nombre: "Short Below Zero", descripcion: "Arctic, islands with snow, broken ice and craters." },
         { nombre: "Tainted Pair" },
+        { nombre: "Tainted Soul", descripcion: "Map size: 257" },
         { nombre: "Tash'Var City", descripcion: "A Tau city. You'll get some help here.<br/>Map size: 1025" },
         { nombre: "Valley of Khorne" },
         { nombre: "Velvet Duress", descripcion: "Archipelago of sand and vegetation in an open sea.<br/>Map size: 257" }
@@ -73,6 +74,7 @@ const MAPAS_CONFIG = {
         { nombre: "Tainted Place" },
         { nombre: "Tainted soul" },
         { nombre: "Tartarus Center" },
+        { nombre: "The Skerries", descripcion: "Dense string of small islands.<br/>Map size: 257" },
         { nombre: "Volcanic Reaction" }
     ],
     "5": [
@@ -92,28 +94,32 @@ const MAPAS_CONFIG = {
         { nombre: "Payne's Retribution", descripcion: "Map size: 513" },
         { nombre: "Pavonis", descripcion: "Map size: 513" },
         { nombre: "Principian Badlands", descripcion: "Map size: 513" },
-        { nombre: "Rhean Floodlands", descripcion: "Map size: 513" },
+        { nombre: "Rhean Floodlands", descripcion: "Treacherous, winding paths, sharp ridges, and sudden gullies.<br/>Map size: 513" },
         { nombre: "Shakun Coast", descripcion: "Map size: 513" },
         { nombre: "Streets of Vogen", descripcion: "Map size: 513" },
         { nombre: "Targorum", descripcion: "Map size: 257" },
         { nombre: "Temple of Change", descripcion: "The worst scenario you will face. Some weird temple." },
         { nombre: "Temple of Cyrene", descripcion: "Ruins of a temple-fortification in cratered terrain.<br/>Map size: 513<br/>Strat. points: 29 | Critical loc.: 3 | Relics: 4 | Slag depos: 5" },
-        { nombre: "Testing Grounds" },
-        { nombre: "Tristam Plains", descripcion: "Map size: 513" },
+        { nombre: "Testing Grounds", descripcion: "Ruins of a temple-fortification in cratered terrain.<br/>Map size: 513" },
+        { nombre: "Tristam Plains", descripcion: "Live combat fighting for Space Marines.<br/>Map size: 513" },
         { nombre: "Trivian Groves", descripcion: "Map size: 513" },
         { nombre: "Vandean Coast", descripcion: "Map size: 257" },
         { nombre: "Western Barrens", descripcion: "Arid rocky desert with a central plateau.<br/>Map size: 513" }
     ],
     "8": [
         { nombre: "Burial Grounds" },
+        { nombre: "Canyon Break", descripcion: "Deep within the canyons of Rynn's World’s arid plateaus, rivers serve as the only natural supply routes." },
         { nombre: "Castle Ruostestein", descripcion: "Map size: 1025<br/>Strat. points: 40 | Critical loc.: 6 | Relics: 8 | Slag depos: 9<br/>This map is thought for a single player with two AI allies, you will defend a castle against 5 AI attackers. The two sides are connected by a small bridge. And two tunnels that can be used by the player 1 only. Don't let the innocent look of that bridge fool you; most of the fight will take place there. Albeit, if you use the tunnels correctly, you will fight in the enemies bases rather than your own. Orks will get 4 Titan cannons and Astra Militarum will get 2 (A key)." },
         { nombre: "Cerulea", descripcion: "Green and rocky terrain with a large central mountain and flowing rivers.<br/>Map size: 513" },
         { nombre: "Daturias Pits" },
         { nombre: "Doom Chamber" },
+        { nombre: "Jalaganda Lowlands", descripcion: "Intricate lattice of canals in this humit industrial wasteland.<br/>Map size: 513" },
         { nombre: "Kierr Harrad", descripcion: "Map size: 512" },
         { nombre: "Lost Hope" },
         { nombre: "Penal Colony" },
-        { nombre: "Rhean Jungle", descripcion: "Dense jungle with open dirt paths and hills.<br/>Map size: 513" }
+        { nombre: "Rhean Jungle", descripcion: "Dense jungle with open dirt paths and hills.<br/>Map size: 513" },
+        { nombre: "The Razor Ridge", descripcion: "Rocky highlands with numerous narrow passes and vertical rock formations." },
+        { nombre: "Wetlands", descripcion: "The dense growth and treacherous vapors of Strank provides lethal negative-cover zones." }
     ]
 };
 
